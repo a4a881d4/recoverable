e38118bf6e6c22ff46e02656948eedd5ee723ba3
a4a881d4 <a4a881d4@163.com>
2018-08-06 00:46:37 +0800

    one solution

--- findError.py
+++ findError.py
@@ -1,6 +1,6 @@
 import prime
 import random
-from sino import Sino
+from sino import Sino,Sino2
 from log import log2
 
 
@@ -9,13 +9,13 @@ p = prime.primeNumber()[-32:]
 h = random.randint(0,2L**256)
 print h
 
-R = Sino.repersentation(h,p)
+R = Sino.repersentation(h,Sino.fromList(p))
 print R
 
 eR = Sino.error(R,p[0],1)
 eR = Sino.error(eR,p[1],4)
 
-eh = Sino.synthesize(eR)[0].r
+eh = Sino.synthesize(eR)
 
 ieh = eh*p[0]
 geh = ieh%Sino.Product(eR)
@@ -44,6 +44,13 @@ ps = ps/(p[1])
 ss = ss%ps
 print log2(ss)
 
+R = Sino.repersentation(h,Sino2.fromList(p))
+print R
+R = Sino.errorList(R,8)
+R = Sino2.checkList(R)
+h2 = Sino.synthesize(R)
+
+print h2
 """
 def down(h,p,r):
 	rn = r[-1]
--- sino.py
+++ sino.py
@@ -1,27 +1,36 @@
+import random
 from gcd import exGCD
 
 class Sino:
-	def __init__(self,p,r):
+	def __init__(self,p):
 		self.p = p
-		self.r = r
+		self.r = 0
+	def put(self,a):
+		self.r = a%self.p
+		return self
+	@staticmethod
+	def fromList(p):
+		return [Sino(pi) for pi in p]
+
 	@staticmethod
 	def repersentation(A,p):
-		return [Sino(pi,A%pi) for pi in p]
+		return [s.put(A) for s in p]
+
 	@staticmethod
 	def merge(a,b):
 		(d,r,l) = exGCD(a.p,b.p)
 		if d!=1:
-			print "no co"
 			p = b.p/d
-			return Sino.merge(a,Sino(p,b.r%p))
+			return Sino.merge(a,Sino(p).put(b.r))
 		else:
 			p = a.p*b.p
-			r = (a.r*l*b.p+b.r*r*a.p)%p
-			return Sino(p,r)
+			r = (a.r*l*b.p+b.r*r*a.p)
+			return Sino(p).put(r)
+	
 	@staticmethod	
 	def synthesize(R):
 		if len(R)==1:
-			return R
+			return R[0].r
 		else:
 			return Sino.synthesize([Sino.merge(R[0],R[1])]+R[2:])
 	
@@ -59,26 +68,71 @@ class Sino:
 			r *= a.p
 		return r
 
+	@staticmethod
+	def errorList(R,n):
+		p = []
+		for s in R:
+			p.append(s.p)
+		for i in range(n):
+			pi = random.choice(p)
+			R = Sino.error(R,pi,random.randint(0,pi-1))
+			print "add error",pi
+		return R
 
+class Sino2(Sino):
+	def __init__(self,p0,p1):
+		self.p0 = p0
+		self.p1 = p1
+		Sino.__init__(self,p0*p1)
+
+	def check(self,a):
+		(d,r,l) = exGCD(a.p,self.p)
+		if d==1:
+			return None
+		else:
+			if self.r%d != a.r%d:
+				return None
+			else:
+				return Sino(d).put(self.r)
 	
+	@staticmethod
+	def fromList(p):
+		return [Sino2(p0,p1) for (p0,p1) in zip(p,p[1:]+p[-1:])]
+	
+	@staticmethod
+	def checkList(R):
+		r = []
+		for i in range(len(R)-1):
+			a = R[i].check(R[i+1])
+			if a!=None:
+				r.append(a)
+		a = R[-1].check(R[0])
+		if a!=None:
+			r.append(a)
+		return r	
 
 def main():
 	import random
 	from prime import primeNumber
 	p = [15,21]
 	A = 33
-	r = Sino.repersentation(A,p)
+	r = Sino.repersentation(A,Sino.fromList(p))
 	print r
 	R = Sino.merge(r[0],r[1])
 	print R
 	A = random.randint(0,2L**256)
 	print A
 	p = primeNumber()[-32:]
-	r = Sino.repersentation(A,p)
+	r = Sino.repersentation(A,Sino.fromList(p))
 	print r
 	R = Sino.synthesize(r)
 	print R
 	print Sino.find(r,p[-1])
+	r = Sino.repersentation(A,Sino2.fromList(p))
+	print r
+	R = Sino.synthesize(r)
+	print R
+	
 
 if __name__ == '__main__':
 	main()
